f5bb1131cc1ecf38a258d6b3154964ceb531baac
Jelmer Snippe <jelmersnippe@hotmail.com>
2026-03-03 19:16:59 +0100

    Add REPL

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use std::fs::read_to_string;
+use std::{
+    fs::read_to_string,
+    io::{self, Write, stdin},
+};
 
 mod automation_engine;
 
 fn main() {
-    process_file("./dsl/variables.dsl");
+    loop {
+        let mut input = String::new();
+        print!("> ");
+        let _ = io::stdout().flush();
+        let _ = stdin().read_line(&mut input);
+        automation_engine::lexer::lexer(input);
+        println!();
+    }
+    // process_file("./dsl/variables.dsl");
 }
 
 fn process_file(path: &'static str) {
     let dsl = read_to_string(path).unwrap();
     println!("Found DSL:\n{dsl}");
-
-    let tokens = automation_engine::lexer::lexer(dsl);
 }
